82ecd78ee38024ce4dc124a9d703d37a4081ee3a
rayGreycloud <rayGreycloud@gmail.com>
2017-01-09 09:52:33 -0800

    Refactor server for production

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config.js');
-
+const path = require('path');
 const app = express();
 
-app.use(webpackMiddleware(webpack(webpackConfig)));
+// Only use webpack if in development not production
+if (process.env.NODE_ENV !== 'production') {
+  // Only import if needed
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack.config.js');
+
+  // In development use webpack
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+  // In production open dist directory
+  app.use(express.static('dist'));
+  // Catch-all route to respond with dist/index.html
+  // Needed for react-router so browser history works
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  })
+}
 
 app.listen(3000, () => console.log('Listening on port 3000'));
